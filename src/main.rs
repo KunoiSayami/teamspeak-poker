038a619b26e68b53d5ec5694bc7c830940a3834b
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2020-11-08 01:44:58 +0800

    Finish program

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,63 @@ use configparser::ini::Ini;
 use std::thread;
 use std::time::Duration;
 
+fn get_value<'a>(option: &'a [&str], index: usize) -> &'a str {
+    option.get(index).unwrap().split('=').last().unwrap()
+}
+
+#[derive(Debug)]
+struct Client {
+    clid: i32,
+    cid: i32,
+    client_database_id: i32,
+    client_nickname: String,
+    client_type: i32
+}
+
+impl Client {
+    fn new(s: &str) -> Client {
+        let options: Vec<&str> = s.split_whitespace().collect();
+        if options.len() != 5 {
+            panic!("")
+        }
+        Client {
+            clid: get_value(&options, 0).parse().unwrap(),
+            cid: get_value(&options, 1).parse().unwrap(),
+            client_database_id: get_value(&options, 2).parse().unwrap(),
+            client_nickname: get_value(&options, 3).parse().unwrap(),
+            client_type: get_value(&options, 4).parse().unwrap()
+        }
+    }
+
+    fn print(&self) {
+        println!("client id: {}, nickname: {}", self.clid, self.client_nickname)
+    }
+}
+
+#[derive(Debug)]
+struct Clients {
+    items: Vec<Client>
+}
+
+impl Clients {
+    fn new(s: String) -> Clients {
+        let mut c = Clients{ items: vec![] };
+        let mut input = s.clone();
+        if input.contains("error id=0 msg=ok") {
+            input = String::from(s.split('\n').next().unwrap());
+        }
+        for client_str in input.split('|') {
+            c.items.push(Client::new(client_str));
+        };
+        c
+    }
+    fn print(&self) {
+        for client in &self.items {
+            client.print()
+        }
+    }
+}
+
 
 fn main() {
     let mut config = Ini::new();
@@ -30,30 +87,82 @@ fn main() {
     let port = config.getint("telnet", "port")
         .expect("Unable found option")
         .unwrap_or(25639);
-    let auth_key = config.get("telnet", "key")
+    let api_key = config.get("telnet", "key")
         .expect("Unable get authorized key");
     let mut telnet = Telnet::connect(("127.0.0.1", port as u16), 512)
         .expect("Couldn't connect to the server...");
 
+    let mut login = false;
+    let mut confirm_login = false;
+    let mut select_client = false;
+    let mut requested_list = false;
+    let clients;
+
     loop {
         let event = telnet.read().expect("Read error");
 
-        match event {
-            TelnetEvent::Data(buffer) => {
-                let s = match String::from_utf8(Vec::from(buffer)) {
-                    Ok(s) => s,
-                    Err(_e) => {
-                        eprintln!("Error while pasting utf8");
-                        continue
-                    }
-                };
-                print!("{}", s);
-                if s.contains(r#"Use the "auth" command to authenticate yourself."#) {
-                    telnet.write(format!("auth apikey={}", auth_key).as_bytes())
-                        .expect("Read error");
+        if let TelnetEvent::Data(buffer) = event {
+            let s = match String::from_utf8(Vec::from(buffer)) {
+                Ok(s) => s,
+                Err(_e) => {
+                    eprintln!("Error while pasting utf8");
+                    continue
                 }
-            },
-            _ => {}
+            };
+            //print!("{}", s);
+            dbg!(s.clone());
+            if !login && s.contains(r#"Use the "auth" command to authenticate yourself."#) {
+                telnet.write(format!("auth apikey={}\n\r", api_key).as_bytes())
+                    .expect("Read error");
+                login = true;
+            } else if !confirm_login && s.contains("error id=0 msg=ok") {
+                telnet.write("use\n\r".as_bytes())
+                    .expect("Read error");
+                confirm_login = true;
+            } else if !select_client && s.contains("selected schandlerid=1") {
+                select_client = true;
+            }
+            if select_client {
+                if !requested_list {
+                    telnet.write("clientlist\n\r".as_bytes())
+                        .expect("Read error");
+                    requested_list = true;
+                } else if s.contains("client_database_id") {
+                    clients = Clients::new(s);
+                    break;
+                }
+            }
         }
+    }
+
+    println!("Client list:");
+    clients.print();
+    println!("Please input which client you want to poke: ");
+    let mut select_user: Option<&Client>;
+    '_outside_loop: loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)
+            .unwrap();
+        let clid: i32 = match input.trim().parse::<i32>() {
+            Ok(id) => id,
+            Err(e) => {
+                println!("Please input a valid number, {:?}: ", e);
+                continue
+            }
+        };
+        for client in &clients.items {
+            if client.clid == clid {
+                select_user = Some(client);
+                break '_outside_loop;
+            }
+        }
+        println!("Id not found, please try again");
+    }
+    let clid = select_user.expect("Couldn't found user").clid;
+    println!("Set clid to {}", clid);
+    for _times in 0..15 {
+        telnet.write(format!("clientpoke msg= clid={}\n\r", clid).as_bytes())
+            .expect("Write error");
+        thread::sleep(Duration::from_millis(1100));
     }
 }
